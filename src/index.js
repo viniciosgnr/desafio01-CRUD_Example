ef233d79debad7825469a5db5e6e7317720512c5
marcosgnr <viniciosgnr@poli.ufrj.br>
2021-07-18 15:19:39 -0300

    conferindo conexão com o github

--- src/index.js
+++ src/index.js
@@ -16,7 +16,7 @@ function checksExistsUserAccount(request, response, next) {
   const user = users.find(user => user.username === username);
 
   if(!user){
-    return response.status(404).json({error: "User not found"});
+    return response.status(404).json({error: "User not found!!"});
   }
 
   request.user = user;
